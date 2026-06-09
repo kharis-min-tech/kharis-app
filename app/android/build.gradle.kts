175ef590056ae623ebdeae496142bf8836dda1fe
Ayodele Ajayi <ayoinc@gmail.com>
2026-06-09 01:13:19 +0100

    fix: force compileSdk 35 for all Android subprojects
    The add_2_calendar plugin was compiled against android-33, but
    androidx dependencies require android-34+. This afterEvaluate
    block forces all subprojects to use compileSdk 35.

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -19,6 +19,17 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android")
+            if (android is com.android.build.gradle.BaseExtension) {
+                android.compileSdkVersion(35)
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
